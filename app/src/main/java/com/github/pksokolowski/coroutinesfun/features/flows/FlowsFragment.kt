aa6c2666544e0cc42840331fcd7a23038818c08f
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2021-01-18 18:18:31 +0100

    add event handling sample
    Comparing livedata (not SingleLiveData) with a sharedFlow configured to
    behave like a SingleLiveData.

--- app/src/main/java/com/github/pksokolowski/coroutinesfun/features/flows/FlowsFragment.kt
+++ app/src/main/java/com/github/pksokolowski/coroutinesfun/features/flows/FlowsFragment.kt
@@ -8,10 +8,16 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.github.pksokolowski.coroutinesfun.databinding.FragmentFlowsBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancelChildren
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class FlowsFragment : Fragment() {
     private val viewModel: FlowsViewModel by viewModels()
+    private val coroutineScope = CoroutineScope(Dispatchers.Main.immediate)
 
     private var _binding: FragmentFlowsBinding? = null
     private val binding get() = _binding!!
@@ -25,6 +31,11 @@ class FlowsFragment : Fragment() {
         return binding.root
     }
 
+    override fun onStop() {
+        super.onStop()
+        coroutineScope.coroutineContext.cancelChildren()
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
@@ -34,12 +45,57 @@ class FlowsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         observeUserData()
+        observeEventsAndState()
         setupOnClickListeners()
+    }
+
+    private fun observeEventsAndState() {
+        viewModel.event.observe(viewLifecycleOwner) {
+            output("observed event")
+        }
+
+        viewModel.state.observe(viewLifecycleOwner) {
+            output("observed state")
+        }
+
+        viewModel.singleEvent
+            .onEach { output("observed single event") }
+            .launchIn(coroutineScope)
+    }
+
+    private fun removeEventsAndStateObservers() {
+        viewModel.event.removeObservers(viewLifecycleOwner)
+        viewModel.state.removeObservers(viewLifecycleOwner)
+        coroutineScope.coroutineContext.cancelChildren()
+    }
+
+    private fun output(content: String?) {
+        val currentOutput = binding.output.text
+        binding.output.text = "$currentOutput\n$content"
     }
 
     private fun setupOnClickListeners() {
         binding.cycleUserData.setOnClickListener {
             viewModel.loadNextUser()
+        }
+
+        binding.sendEvent.setOnClickListener {
+            viewModel.sendEvent("event livedata observed")
+        }
+
+        binding.setState.setOnClickListener {
+            viewModel.setState("some new state observed")
+        }
+
+        binding.sendSingleEvent.setOnClickListener {
+            viewModel.sendSingleEvent("new single event")
+        }
+
+        binding.reSubscribe.setOnClickListener {
+            binding.output.text = ""
+            output("simulating view re-creation")
+            removeEventsAndStateObservers()
+            observeEventsAndState()
         }
     }
 
--- app/src/main/java/com/github/pksokolowski/coroutinesfun/features/flows/FlowsViewModel.kt
+++ app/src/main/java/com/github/pksokolowski/coroutinesfun/features/flows/FlowsViewModel.kt
@@ -4,12 +4,41 @@ import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
 
 class FlowsViewModel @ViewModelInject constructor(
     private val usersProvider: UsersProvider
 ) : ViewModel() {
     val currentUser = usersProvider.user.asLiveData()
+
+    private val _singleEvent = MutableSharedFlow<String>(replay = 0, extraBufferCapacity = 0)
+    val singleEvent = _singleEvent.asSharedFlow()
+
+    private val _event =
+        MutableSharedFlow<String>(replay = 0, extraBufferCapacity = 0)
+    val event = _event.asLiveData()
+
+    private val _state = MutableStateFlow("initial state")
+    val state = _state.asLiveData()
+
+    fun sendSingleEvent(content: String) {
+        viewModelScope.launch {
+            _singleEvent.emit(content)
+        }
+    }
+
+    fun sendEvent(content: String) {
+        viewModelScope.launch {
+            _event.emit(content)
+        }
+    }
+
+    fun setState(content: String) {
+        _state.value = content
+    }
 
     fun loadNextUser() {
         viewModelScope.launch {
