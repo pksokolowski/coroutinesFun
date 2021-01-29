3f3a1808d63dd3de86f6b2efb29ea3a0aea4e6dd
Paweł Sokołowski <p.k.sokolowski@outlook.com>
2021-01-29 23:56:46 +0100

    add alternative version of double-tap operator

--- app/src/main/java/com/github/pksokolowski/coroutinesfun/features/standalones/CustomOperators.kt
+++ app/src/main/java/com/github/pksokolowski/coroutinesfun/features/standalones/CustomOperators.kt
@@ -3,6 +3,7 @@ package com.github.pksokolowski.coroutinesfun.features.standalones
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.transform
 
 fun <T> Flow<T>.filterDoubleTap(periodMillis: Long): Flow<Unit> {
     require(periodMillis > 0) { "Double tap acceptance period should be positive" }
@@ -16,6 +17,24 @@ fun <T> Flow<T>.filterDoubleTap(periodMillis: Long): Flow<Unit> {
                 emit(Unit)
                 lastStamp = -1
             }
+        }
+    }
+}
+
+/**
+ * An alternative implementation of the same [filterDoubleTap] operator, here using
+ * transform {} instead of an explicit flow { collect { } } combo.
+ */
+fun <T> Flow<T>.filterDoubleTapAlternative(periodMillis: Long): Flow<Unit> {
+    require(periodMillis > 0) { "Double tap acceptance period should be positive" }
+    var lastStamp = -1L
+    return transform {
+        val now = System.currentTimeMillis()
+        val sinceLast = now - lastStamp
+        lastStamp = now
+        if (sinceLast <= periodMillis) {
+            emit(Unit)
+            lastStamp = -1
         }
     }
 }
